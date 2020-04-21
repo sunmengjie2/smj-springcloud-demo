939553368e598e541714ae1aead7f7cf9e22d615
sunmengjie2 <1004539656@qq.com>
2020-04-21 18:09:49 +0800

    添加user服务，order服务，并注册到eureka-server

--- service-order/src/main/java/com/sunmj/springcloud/order/controller/OrderController.java
+++ service-order/src/main/java/com/sunmj/springcloud/order/controller/OrderController.java
@@ -2,10 +2,13 @@ package com.sunmj.springcloud.order.controller;
 
 import com.sunmj.springcloud.order.model.Order;
 import com.sunmj.springcloud.order.service.OrderService;
+import com.sunmj.springcloud.user.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/order/")
@@ -14,6 +17,9 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     /**
      * 根据id查询订单
      * @param id
@@ -21,8 +27,9 @@ public class OrderController {
      */
     @RequestMapping("getOrderById")
     public Order getOrderById(@RequestParam("id") Integer id){
+        Order order = orderService.getOrderById(id);
 
-        return orderService.getOrderById(id);
+        return order;
     }
 
 
